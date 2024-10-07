5efea547ea33c9a4b696dd2926008aa41a1e486d
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-10-07 19:52:37 -0400

    jkgjkhg

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ const students = [
     { name: 'Diana', age: 23, grade: 95 },
   ];
 
-students.forEach((student)=>console.log(student.name + ";" + student.grade));
+//  1. Log each student's name and grade
+students.forEach((student) => console.log(student.name + ";" + student.grade));
+
+//  1. Filter students with grades above 80
+const topStudents = students.filter((student) => student.grade > 80);
+
+//  2. Filter students who are 21 or younger
+const youngStudents = students.filter((student) => student.age <= 21);
+
+//check work
+console.log(topStudents + ";" + youngStudents);
+
