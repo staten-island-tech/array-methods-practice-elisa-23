caafe6ac8e7e320f7457a69401515aafc45aa95d
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-10-07 19:32:06 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const students = [
+    { name: 'Alice', age: 20, grade: 85 },
+    { name: 'Bob', age: 22, grade: 90 },
+    { name: 'Charlie', age: 21, grade: 78 },
+    { name: 'Diana', age: 23, grade: 95 },
+  ];
+
+students.forEach((student)=>console.log(student.name + ";" + student.grade));
