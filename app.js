765910380be8c6c447b447a23e08f96a1545c9f0
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-10-08 12:09:09 -0400

    rthr

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const students = [
     { name: 'Diana', age: 23, grade: 95 },
 ];
 
+
+
 //  1. Log each student's name and grade.
 students.forEach((student) => console.log(student.name + ";" + student.grade));
 
@@ -15,6 +17,8 @@ console.log(topStudents);
 //  2. Filter students who are 21 or younger.
 const youngStudents = students.filter((student) => student.age <= 21);
 console.log(youngStudents);
+
+
 
 //  1. Log the names of students who scored above 80.
 students
@@ -36,10 +40,45 @@ const products = [
     { name: 'Desk', price: 450, category: 'Furniture', rating: 4.8 }
 ];
 
+
+
 //  1. Display Project Details: Log the name and price of each product.
 products.forEach((product)=> console.log(product.name + ";" + product.price));
 
 //  2. Increase Price: Increase the price of each project by 10% and log the updated products.
 products.forEach((product)=> console.log(product.name + ";" + (product.price * 1.1)));
 
-//  3.
+//  3. Summarize Categories: Use forEach to create a list of all unqiue categories in the product array.
+const categories = [];
+products.forEach((product)=> {
+  if (!categories.includes(product.category)) {
+    categories.push(product.category);
+  }
+});
+console.log(categories);
+
+
+
+//  1. Filter by Category: Create a new array that only includes products from the 'Electronics' category.
+const electronics = products.filter((product)=> product.category === 'Electronics');
+console.log(electronics);
+
+//  2. Filter by Price: Filter products that cost more than $300 and store them in a new array.
+const expensiveProducts = products.filter((product) => product.price > 300);
+console.log(expensiveProducts);
+
+//  3. Highly Rated Products. Filter products with a rating of 4.5 or above.
+const goodProducts = products.filter((product) => product.rating >= 4.5);
+console.log(goodProducts);
+
+
+
+//  1. Log Highly Rated Product Names: Use filter to get the highly rated products (rating >= 4.5) amd then use forEach to log only their names.
+products
+  .filter((product) => product.rating >= 4.5)
+  .forEach((product) => console.log(product.name));
+
+//  2. Affordable Electronics: Use filter to find all the products in the 'Electronics' category that are placed below $1000. After filtering, use forEach to log their details.
+products
+  .filter((product) => product.price < 1000)
+  .forEach((product) => console.log(product));
